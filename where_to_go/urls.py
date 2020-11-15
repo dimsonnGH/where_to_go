b5781c3fe6b2ea46885a052bf9e8f6e7589c9267
dimsonnGH <dimsonn@mail.ru>
2020-11-15 19:36:48 +0400

    Create json from DB

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('places/<slug:placeId>', views.place_detail),
+    path('places/<slug:placeId>', views.place_json),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from where_to_go.settings import STATIC_URL
-from places.models import Place
+from places.models import Place, PlaceImage
 
 
 def index(request) :
@@ -29,7 +29,18 @@ def index(request) :
     return render(request, 'index.html', data)
 
 
-def place_detail(request, placeId):
+def place_json(request, placeId) :
     place = get_object_or_404(Place, placeId=placeId)
+    images = PlaceImage.objects.filter(place=place)
+    data = {
+        "title" : place.title,
+        "imgs" : [image.image.url for image in images],
+        "description_short" : place.description_short,
+        "description_long" : place.description_long,
+        "coordinates" : {
+            "lng" : place.longitude,
+            "lat" : place.latitude
+        }
+    }
 
-    return HttpResponse(place.title)
+    return JsonResponse(data, json_dumps_params={'ensure_ascii': False})
