ca616008923b6857c6f9d0037a51a1c5ed7d4802
dimsonnGH <dimsonn@mail.ru>
2020-11-15 10:21:35 +0400

    Create API Endpoint

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('places/<slug:placeId>', views.place_detail),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from where_to_go.settings import STATIC_URL
 from places.models import Place
 
@@ -27,3 +27,9 @@ def index(request) :
     data = {"places_geojson" : places_geojson}
 
     return render(request, 'index.html', data)
+
+
+def place_detail(request, placeId):
+    place = get_object_or_404(Place, placeId=placeId)
+
+    return HttpResponse(place.title)
