9589190b6726eea52805eb403d1c6905dd04c27b
dimsonnGH <dimsonn@mail.ru>
2021-07-31 22:24:04 +0400

    remove excess "try ... except"

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -9,49 +9,46 @@ from urllib.parse import urlparse, unquote
 import os.path
 
 
-class Command(BaseCommand) :
+class Command(BaseCommand):
     help = 'Load json data file to DB'
 
-    def add_arguments(self, parser) :
+    def add_arguments(self, parser):
         parser.add_argument('url', type=str)
 
-    def handle(self, *args, **options) :
+    def handle(self, *args, **options):
         url = options['url']
-            response = requests.get(url)
+        response = requests.get(url)
+        response.raise_for_status()
+        loaded_place = response.json()
+
+    place, created = Place.objects.get_or_create(
+        title=loaded_place['title'],
+        defaults={
+            'place_id': slugify(translit(loaded_place['title'][:100], 'ru', reversed=True)),
+            'description_short': loaded_place['description_short'],
+            'description_long': loaded_place['description_long'],
+            'longitude': loaded_place['coordinates']['lng'],
+            'latitude': loaded_place['coordinates']['lat'],
+        }
+    )
+
+    if not created:
+        return
+
+    for order, img_url in enumerate(loaded_place['imgs'], 1):
+        try:
+            response = requests.get(img_url)
             response.raise_for_status()
-            loaded_place = response.json()
-        except Exception :
+
+            content_file = ContentFile(response.content)
+
+            place_img = PlaceImage(place=place, order=order)
+
+            url_parts = urlparse(img_url)
+            img_path = url_parts.path
+            (_, img_name) = os.path.split(img_path)
+            img_name = unquote(img_name)
+
+            place_img.image.save(img_name, content_file, True)
+        except Exception:
             logging.exception('')
-            return
-
-        place, created = Place.objects.get_or_create(
-            title=loaded_place['title'],
-            defaults={
-                'place_id' : slugify(translit(loaded_place['title'][:100], 'ru', reversed=True)),
-                'description_short' : loaded_place['description_short'],
-                'description_long' : loaded_place['description_long'],
-                'longitude' : loaded_place['coordinates']['lng'],
-                'latitude' : loaded_place['coordinates']['lat'],
-            }
-        )
-
-        if not created :
-            return
-
-        for order, img_url in enumerate(loaded_place['imgs'], 1) :
-            try :
-                response = requests.get(img_url)
-                response.raise_for_status()
-
-                content_file = ContentFile(response.content)
-
-                place_img = PlaceImage(place=place, order=order)
-
-                url_parts = urlparse(img_url)
-                img_path = url_parts.path
-                (_, img_name) = os.path.split(img_path)
-                img_name = unquote(img_name)
-
-                place_img.image.save(img_name, content_file, True)
-            except Exception :
-                logging.exception('')
